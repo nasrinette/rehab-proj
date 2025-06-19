bdfa21cb6fca5ffb18a17ff5a3558ea056867006
YashBhartia00 <51514725+YashBhartia00@users.noreply.github.com>
2025-06-19 21:08:03 +0200

    recordings and playback with public ui function

--- Assets/Scripts/MovementRecordings/MovementRecordingsManager.cs
+++ Assets/Scripts/MovementRecordings/MovementRecordingsManager.cs
@@ -4,18 +4,38 @@ using System.IO;
 using UnityEngine;
 using System;
 
+
+public class FeedbackList
+{
+    public List<string> timestamps;
+}
+
 public class MovementRecordingsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public FeedbackList MakeFeedbackListFromFiles(string exerciseName) // exercisename should be like "Patient_DrawingExercise" or "Doctor_DrawingExercise"
     {
-        
-    }
+        string timestampFromFile;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "RehabProject", "Feedbacks");
+        if (!Directory.Exists(folderPath))
+        {
+            Debug.LogWarning("Directory does not exist: " + folderPath);
+            return null;
+        }
+
+        string[] files = Directory.GetFiles(folderPath, exerciseName + "*.csv");
+        FeedbackList feedbacksList = new FeedbackList();
+
+        foreach (string file in files)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(file);
+            
+            // if filename starts with patient_ or doctor_ then get the timestamp which is the part after 2nd _ otherwise it is after the first 
+            timestampFromFile = fileName.Contains("octor") || fileName.Contains("atient") ? fileName.Split('_')[2] : fileName.Split('_')[1];
+            feedbacksList.timestamps.Add(timestampFromFile);
+        }
+        return feedbacksList;
     }
 
     public ExerciseList MakeExerciseListFromFiles(bool patient)
